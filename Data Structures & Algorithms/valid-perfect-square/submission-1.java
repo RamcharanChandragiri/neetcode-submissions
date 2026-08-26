ab4c7cecbd140927a95c01426c40c1f8da5c5be4
RamcharanChandragiri <ramcharanch06@gmail.com>
2026-08-26 21:50:04 +0530

    Add: valid-perfect-square - submission-1

---
+++ Data Structures & Algorithms/valid-perfect-square/submission-1.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        if(num<2) return true;
+        for(int i=1;i<=num;i++){
+            if(i*i==num)
+                return true;
+        }
+        return false;
+    }
+}
